f54ded7a72f30c5654cb62a37a02f432b7745bbb
Roman <kaboom.ua@gmail.com>
2017-02-15 23:05:13 +0200

    Update filterByDate.js

--- src/reducer/filterByDate.js
+++ src/reducer/filterByDate.js
@@ -1,5 +1,6 @@
 import {FILTER_BY_DATE} from '../constants'
 
+//не дроби редюсеры слишком сильно: объедини селект и календарь в один
 export default (dateRange = null, action) => {
     const {type, payload} = action
 
