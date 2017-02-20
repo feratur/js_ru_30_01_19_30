c386eeb205c3f80816c7898c8803fe5285b7c1eb
Roman <kaboom.ua@gmail.com>
2017-02-20 11:09:58 +0200

    Update idGenerator.js

--- src/middlewares/idGenerator.js
+++ src/middlewares/idGenerator.js
@@ -3,9 +3,11 @@ import {ADD_COMMENT} from '../constants'
 
 export default store => next => action => {
     switch (action.type){
+	//через мидлвары будут проходить все экшины, суть в том, что делать их нужно максимально реюзабильными. Не завязывайся на один экшин
         case ADD_COMMENT:
             const maxId = mapToArr(store.getState().comments)
                 .map(x => x.id)
+                //можно заменить на Math.max, но еще лучше просто рендомный генерить, а то гарантии уникальности слабые
                 .reduce((prev, current) => prev < current ? current : prev, 0)
 
             action.payload.id = maxId + 1
